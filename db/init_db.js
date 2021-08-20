45c2e3829a296158a03896afe99036182e533038
Burk Manson <burkmanson@gmail.com>
2021-08-19 22:02:10 -0700

    Added verbosity

--- db/init_db.js
+++ db/init_db.js
@@ -4,6 +4,8 @@ const { initialPosts, initialProjects } = require('./initialData.json');
 
 async function buildTables() {
   try {
+    console.log('Rebuilding database');
+
     await client.connect();
 
     await client.query(`
@@ -32,10 +34,12 @@ async function buildTables() {
 }
 
 async function initializePosts() {
+  console.log('Intializing posts');
   await Promise.all(initialPosts.map(createPost));
 }
 
 async function initializeProjects() {
+  console.log('Intializing projects');
   await Promise.all(initialProjects.map(createProject));
 }
 
@@ -43,4 +47,7 @@ buildTables()
   .then(initializePosts)
   .then(initializeProjects)
   .catch(console.error)
-  .finally(() => client.end());
+  .finally(() => {
+    console.log('Finished initializing database');
+    client.end();
+  });
