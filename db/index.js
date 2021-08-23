d65e18caf228cd0b86e883f22c818858d4b5cad4
Burk Manson <burkmanson@gmail.com>
2021-08-22 21:20:46 -0700

    Added db functions

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
-const bcrypt = require('bcrypt');
 const DB_NAME = 'iamburk';
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-
 const client = new Client(DB_URL);
+
+const bcrypt = require('bcrypt');
 
 // POSTS
 async function getPosts() {
@@ -40,6 +40,49 @@ async function createPost({ title, content }) {
   }
 }
 
+async function updatePost(id, fields = {}) {
+  if (Object.keys(fields).length === 0) {
+    return false;
+  }
+
+  const setString = Object.keys(fields)
+    .map((key, index) => {
+      return `"${key}"=$${index + 1}`;
+    })
+    .join(', ');
+
+  try {
+    await client.query(
+      `
+        UPDATE posts
+        SET ${setString}
+        WHERE id=${id}
+      `,
+      Object.values(fields)
+    );
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deletePost({ id }) {
+  try {
+    await client.query(
+      `
+        DELETE FROM posts
+        WHERE id=$1;
+      `,
+      [id]
+    );
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
 // PROJECTS
 async function getProjects() {
   try {
@@ -69,6 +112,49 @@ async function createProject({ title, content, deploy, github }) {
     );
 
     return project;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateProject(id, fields = {}) {
+  if (Object.keys(fields).length === 0) {
+    return false;
+  }
+
+  const setString = Object.keys(fields)
+    .map((key, index) => {
+      return `"${key}"=$${index + 1}`;
+    })
+    .join(', ');
+
+  try {
+    await client.query(
+      `
+        UPDATE projects
+        SET ${setString}
+        WHERE id=${id}
+      `,
+      Object.values(fields)
+    );
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteProject({ id }) {
+  try {
+    await client.query(
+      `
+        DELETE FROM projects
+        WHERE id=$1;
+      `,
+      [id]
+    );
+
+    return true;
   } catch (error) {
     throw error;
   }
@@ -123,12 +209,66 @@ async function loginUser({ username, password }) {
   }
 }
 
+async function updateUser(uuid, fields = {}) {
+  if (Object.keys(fields).length === 0) {
+    return false;
+  } else if ('password' in fields) {
+    fields.password = bcrypt.hashSync(fields.password, 10);
+  }
+
+  const setString = Object.keys(fields)
+    .map((key, index) => {
+      return `"${key}"=$${index + 1}`;
+    })
+    .join(', ');
+
+  try {
+    await client.query(
+      `
+        UPDATE users
+        SET ${setString}
+        WHERE uuid=${uuid}
+      `,
+      Object.values(fields)
+    );
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteUser({ uuid }) {
+  try {
+    await client.query(
+      `
+        DELETE FROM users
+        WHERE uuid=$1;
+      `,
+      [uuid]
+    );
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
+  // POSTS
   getPosts,
   createPost,
+  updatePost,
+  deletePost,
+  // PROJECTS
   getProjects,
   createProject,
+  updateProject,
+  deleteProject,
+  // USERS
   createUser,
   loginUser,
+  updateUser,
+  deleteUser,
 };
--- db/init_db.js
+++ db/init_db.js
@@ -1,4 +1,4 @@
-const { client, createPost, createProject } = require('./index.js');
+const { client, createPost, createProject, createUser } = require('./index.js');
 
 const { initialPosts, initialProjects } = require('./initialData.json');
 
@@ -9,33 +9,34 @@ async function buildTables() {
     await client.connect();
 
     await client.query(`
-        DROP TABLE IF EXISTS posts;
-        DROP TABLE IF EXISTS projects;
-        DROP TABLE IF EXISTS users;
-        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+      DROP TABLE IF EXISTS posts;
+      DROP TABLE IF EXISTS projects;
+      DROP TABLE IF EXISTS users;
+      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     `);
 
     await client.query(`
-        CREATE TABLE posts(
-          id SERIAL PRIMARY KEY,
-          title VARCHAR(255) NOT NULL,
-          content TEXT
-        );
+      CREATE TABLE posts(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        content TEXT
+      );
 
-        CREATE TABLE projects(
-          id SERIAL PRIMARY KEY,
-          title VARCHAR(255) NOT NULL,
-          content TEXT,
-          deploy TEXT,
-          github TEXT
-        );
+      CREATE TABLE projects(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        content TEXT,
+        deploy TEXT,
+        github TEXT
+      );
 
-        CREATE TABLE users(
-          uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-          username VARCHAR(255) UNIQUE NOT NULL,
-          password TEXT NOT NULL,
-          isadmin BOOLEAN DEFAULT false
-        );
+      CREATE TABLE users(
+        uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        isadmin BOOLEAN DEFAULT false,
+        roles TEXT[] DEFAULT '{}'
+      );
     `);
   } catch (error) {
     throw error;
@@ -52,9 +53,16 @@ async function initializeProjects() {
   await Promise.all(initialProjects.map(createProject));
 }
 
+async function initializeUsers() {
+  console.log('Intializing users');
+  const initialUsers = [{ username: 'iamburk', password: 'nicetry' }];
+  await Promise.all(initialUsers.map(createUser));
+}
+
 buildTables()
   .then(initializePosts)
   .then(initializeProjects)
+  .then(initializeUsers)
   .catch(console.error)
   .finally(() => {
     console.log('Finished initializing database');
